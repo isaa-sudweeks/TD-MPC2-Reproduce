64909b275bd3c2f9e927a053ef70552cb122144a
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2026-03-31 18:25:14 -0600

    feat: uncomment environment registration imports for dmcontrol, maniskill, metaworld, and myosuite

--- TD-MPC2/envs/__init__.py
+++ TD-MPC2/envs/__init__.py
@@ -5,22 +5,22 @@ import gymnasium as gym
 
 # TODO: Add support for multitask envs 
 
-# try:
-# 	from envs.dmcontrol import make_env as make_dm_control_env
-# except:
-# 	make_dm_control_env = missing_dependencies
-# try:
-# 	from envs.maniskill import make_env as make_maniskill_env
-# except:
-# 	make_maniskill_env = missing_dependencies
-# try:
-# 	from envs.metaworld import make_env as make_metaworld_env
-# except:
-# 	make_metaworld_env = missing_dependencies
-# try:
-# 	from envs.myosuite import make_env as make_myosuite_env
-# except:
-# 	make_myosuite_env = missing_dependencies
+try:
+	from envs.dmcontrol import make_env as make_dm_control_env
+except:
+	make_dm_control_env = missing_dependencies
+try:
+	from envs.maniskill import make_env as make_maniskill_env
+except:
+	make_maniskill_env = missing_dependencies
+try:
+	from envs.metaworld import make_env as make_metaworld_env
+except:
+	make_metaworld_env = missing_dependencies
+try:
+	from envs.myosuite import make_env as make_myosuite_env
+except:
+	make_myosuite_env = missing_dependencies
 try:
 	from envs.mujoco import make_env as make_mujoco_env
 except:
