8a21a40a52891d85a723cdbeff92e56e0b6b46c9
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2026-04-07 11:40:33 -0600

    feat: implement parallel multi-task evaluation and training using ThreadPoolExecutor

--- TD-MPC2/trainer/online_multitask_trainer.py
+++ TD-MPC2/trainer/online_multitask_trainer.py
@@ -26,31 +26,86 @@ class OnlineMultitaskTrainer(Trainer):
         )
     
     def eval(self):
+        import concurrent.futures
         results = dict()
-        for task_idx in tqdm(range(len(self.cfg.tasks)), desc="Evaluating"):
-            ep_rewards, ep_successes, ep_lengths = [], [], []
-            for i in range(int(self.cfg.eval_episodes)):
-                obs, done, ep_reward, t = self.env.reset(task_idx=task_idx), False, 0, 0
-                if self.cfg.save_video:
+        num_tasks = len(self.cfg.tasks)
+         
+        # Run sequentially if video is enabled because VideoRecorder only tracks 1 env at a time
+        if self.cfg.save_video:
+            for task_idx in tqdm(range(len(self.cfg.tasks)), desc="Evaluating (Sequential for Video)"):
+                ep_rewards, ep_successes, ep_lengths = [], [], []
+                for i in range(int(self.cfg.eval_episodes)):
+                    obs, done, ep_reward, t = self.env.reset(task_idx=task_idx), False, 0, 0
                     self.logger.video.init(self.env, enabled=(i==0))
-                while not done:
-                    if getattr(self.cfg, 'device', 'cuda') == 'cuda':
-                        torch.compiler.cudagraph_mark_step_begin()
-                    action = self.agent.act(obs, t0=t==0, eval_mode=True, task=task_idx)
-                    obs, reward, done, info = self.env.step(action)
-                    ep_reward += reward
-                    t += 1
-                    if self.cfg.save_video:
+                    while not done:
+                        if getattr(self.cfg, 'device', 'cuda') == 'cuda':
+                            torch.compiler.cudagraph_mark_step_begin()
+                        action = self.agent.act(obs, t0=t==0, eval_mode=True, task=task_idx)
+                        obs, reward, done, info = self.env.step(action)
+                        ep_reward += reward
+                        t += 1
                         self.logger.video.record(self.env)
-                ep_rewards.append(ep_reward)
-                ep_successes.append(info['success'])
-                ep_lengths.append(t)
-                if self.cfg.save_video:
+                    ep_rewards.append(ep_reward)
+                    ep_successes.append(info['success'])
+                    ep_lengths.append(t)
                     self.logger.video.save(self._step, key=f'videos/eval_video_{self.cfg.tasks[task_idx]}')
-            results.update({
-                f'episode_reward+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_rewards),
-                f'episode_success+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_successes),
-            })
+                results.update({
+                    f'episode_reward+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_rewards),
+                    f'episode_success+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_successes),
+                })
+        else:
+            ep_rewards = [[] for _ in range(num_tasks)]
+            ep_successes = [[] for _ in range(num_tasks)]
+            print("Evaluating 4 tasks in parallel...")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=num_tasks) as executor:
+                for _ in range(int(self.cfg.eval_episodes)):
+                    obs = [self.env.envs[task_idx].reset() for task_idx in range(num_tasks)]
+                    dones = [False] * num_tasks
+                    ep_reward = [0.0] * num_tasks
+                    t = [0] * num_tasks
+                    infos = [None] * num_tasks
+                    prev_means = [torch.zeros(self.cfg.horizon, self.cfg.action_dim, device=self.agent.device) for _ in range(num_tasks)]
+                    
+                    while not all(dones):
+                        actions = []
+                        for task_idx in range(num_tasks):
+                            if dones[task_idx]:
+                                actions.append(None)
+                                continue
+                            
+                            if getattr(self.cfg, 'device', 'cuda') == 'cuda':
+                                torch.compiler.cudagraph_mark_step_begin()
+                                
+                            self.agent._prev_mean.copy_(prev_means[task_idx])
+                            action = self.agent.act(obs[task_idx], t0=t[task_idx]==0, eval_mode=True, task=task_idx)
+                            prev_means[task_idx].copy_(self.agent._prev_mean)
+                            actions.append(action)
+                        
+                        futures = []
+                        for task_idx in range(num_tasks):
+                            if not dones[task_idx]:
+                                futures.append(executor.submit(self.env.envs[task_idx].step, actions[task_idx]))
+                            else:
+                                futures.append(None)
+                                
+                        for task_idx in range(num_tasks):
+                            if futures[task_idx] is not None:
+                                _obs, _reward, _done, _info = futures[task_idx].result()
+                                obs[task_idx] = _obs
+                                ep_reward[task_idx] += _reward
+                                t[task_idx] += 1
+                                infos[task_idx] = _info
+                                dones[task_idx] = _done
+                                
+                    for task_idx in range(num_tasks):
+                        ep_rewards[task_idx].append(ep_reward[task_idx])
+                        ep_successes[task_idx].append(infos[task_idx]['success'])
+            
+            for task_idx in range(num_tasks):
+                results.update({
+                    f'episode_reward+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_rewards[task_idx]),
+                    f'episode_success+{self.cfg.tasks[task_idx]}' : np.nanmean(ep_successes[task_idx]),
+                })
         
         if len(self.cfg.tasks) > 0:
             results['episode_reward'] = np.nanmean([results[f'episode_reward+{self.cfg.tasks[i]}'] for i in range(len(self.cfg.tasks))])
@@ -81,57 +136,84 @@ class OnlineMultitaskTrainer(Trainer):
         return td
 
     def train(self):
-        train_metrics, done, eval_next = {}, True, False 
-        task_idx = 0
-        while self._step <= self.cfg.steps:
-            if self._step % self.cfg.eval_freq == 0:
-                eval_next = True 
-            
-            if done:
-                if eval_next:
-                    eval_metrics = self.eval()
-                    eval_metrics.update(self.common_metrics())
-                    self.logger.log(eval_metrics, 'eval')
-                    self.report_eval_metrics(eval_metrics, self._step)
-                    eval_next = False
-                if self._step > 0:
-                    log_metrics = train_metrics.copy()
-                    ep_reward = torch.tensor([td['reward'] for td in self._tds[1:]]).sum()
-                    log_metrics.update({
-                        f'episode_reward+{self.cfg.tasks[task_idx]}': ep_reward,
-                        f'episode_success+{self.cfg.tasks[task_idx]}': info['success'],
-                        f'episode_length+{self.cfg.tasks[task_idx]}': len(self._tds),
-                        f'episode_terminated+{self.cfg.tasks[task_idx]}': info['terminated'],
-                        'episode_reward': ep_reward,
-                        'episode_success': info['success'],
-                    })
-                    log_metrics.update(self.common_metrics())
-                    self.logger.log(log_metrics, 'train')
-                    train_metrics = {}
-                    self._ep_idx = self.buffer.add(torch.cat(self._tds))
-
-                task_idx = np.random.randint(len(self.cfg.tasks))
-                obs = self.env.reset(task_idx=task_idx)
-                self._tds = [self.to_td(obs, task_idx=task_idx)]
-            
-            if self._step > self.cfg.seed_steps:
-                action = self.agent.act(obs, t0=len(self._tds)==1, task=task_idx)
-            else:
-                action = self.env.rand_act()
-            obs, reward, done, info = self.env.step(action)
-            terminated = info['terminated'] if self.cfg.episodic else torch.tensor(0.0)
-            self._tds.append(self.to_td(obs, action, reward, terminated, task_idx))
-
-            if self._step >= self.cfg.seed_steps:
-                if self._step == self.cfg.seed_steps:
-                    num_updates = self.cfg.seed_steps
-                    print('Pretraining agent on seed data...')
-                else:
-                    num_updates = 1
-                for _ in range(int(num_updates)):
-                    _train_metrics = self.agent.update(self.buffer)
-                train_metrics.update(_train_metrics)
-
-            self._step += 1
+        import concurrent.futures
+        train_metrics, eval_next = {}, False 
+        
+        num_tasks = len(self.cfg.tasks)
+        dones = [True] * num_tasks
+        obs_list = [None] * num_tasks
+        tds_list = [[] for _ in range(num_tasks)]
+        infos_list = [None] * num_tasks
+        prev_means = [torch.zeros(self.cfg.horizon, self.cfg.action_dim, device=self.agent.device) for _ in range(num_tasks)]
+        
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_tasks) as executor:
+            while self._step <= self.cfg.steps:
+                if self._step > 0 and self._step % self.cfg.eval_freq < num_tasks:
+                    eval_next = True 
+                
+                # Handle reset and evaluation at the start of any new episode
+                for i in range(num_tasks):
+                    if dones[i]:
+                        if eval_next and i == 0:
+                            eval_metrics = self.eval()
+                            eval_metrics.update(self.common_metrics())
+                            self.logger.log(eval_metrics, 'eval')
+                            self.report_eval_metrics(eval_metrics, self._step)
+                            eval_next = False
+                        if len(tds_list[i]) > 0:
+                            if self._step > 0:
+                                log_metrics = train_metrics.copy()
+                                ep_reward = torch.tensor([td['reward'] for td in tds_list[i][1:]]).sum()
+                                log_metrics.update({
+                                    f'episode_reward+{self.cfg.tasks[i]}': ep_reward,
+                                    f'episode_success+{self.cfg.tasks[i]}': infos_list[i]['success'],
+                                    f'episode_length+{self.cfg.tasks[i]}': len(tds_list[i]),
+                                    f'episode_terminated+{self.cfg.tasks[i]}': infos_list[i]['terminated'],
+                                    'episode_reward': ep_reward,
+                                    'episode_success': infos_list[i]['success'],
+                                })
+                                log_metrics.update(self.common_metrics())
+                                self.logger.log(log_metrics, 'train')
+                                train_metrics = {}
+                                self._ep_idx = self.buffer.add(torch.cat(tds_list[i]))
+                        
+                        obs_list[i] = self.env.envs[i].reset()
+                        tds_list[i] = [self.to_td(obs_list[i], task_idx=i)]
+                        prev_means[i].zero_()
+                        dones[i] = False
+                
+                # Act simultaneously
+                actions = []
+                for i in range(num_tasks):
+                    if self._step > self.cfg.seed_steps:
+                        self.agent._prev_mean.copy_(prev_means[i])
+                        action = self.agent.act(obs_list[i], t0=(len(tds_list[i])==1), task=i)
+                        prev_means[i].copy_(self.agent._prev_mean)
+                    else:
+                        self.env.active_env_idx = i
+                        action = self.env.rand_act()
+                    actions.append(action)
+                
+                # Step environments concurrently via ThreadPoolExecutor
+                futures = [executor.submit(self.env.envs[i].step, actions[i]) for i in range(num_tasks)]
+                
+                for i in range(num_tasks):
+                    obs, reward, done, info = futures[i].result()
+                    obs_list[i], dones[i], infos_list[i] = obs, done, info
+                    terminated = info['terminated'] if self.cfg.episodic else torch.tensor(0.0)
+                    tds_list[i].append(self.to_td(obs, actions[i], reward, terminated, i))
+    
+                if self._step >= self.cfg.seed_steps:
+                    if self._step - num_tasks < self.cfg.seed_steps:
+                        num_updates = self.cfg.seed_steps
+                        print('Pretraining agent on seed data...')
+                    else:
+                        num_updates = num_tasks  # maintain 1 update per env step collected
+                    for _ in range(int(num_updates)):
+                        _train_metrics = self.agent.update(self.buffer)
+                    train_metrics.update(_train_metrics)
+    
+                self._step += num_tasks
+                
         self.logger.finish(self.agent)
         return self._best_eval_metrics
